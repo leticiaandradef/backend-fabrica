f2f382aa233074aef986341f155594a194a3d754
leticiaandradef <lelectpm2021@gmail.com>
2025-07-09 21:24:15 -0400

    teste2

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { getResumoPedido } from './servico/resumo.js';
+import registrarResumoPedido from './servico/resumoPedido.js';
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/resumo/:idCliente', async (req, res) => {
   const { idCliente } = req.params;
@@ -21,7 +23,7 @@ app.get('/resumo/:idCliente', async (req, res) => {
 
   } catch (error) {
     console.error('Erro na rota /resumo:', error.message);
-    
+
     const isClientError = error.message.includes('Nenhum pedido') || error.message.includes('ID do cliente');
 
     res.status(isClientError ? 400 : 500).json({
@@ -30,6 +32,30 @@ app.get('/resumo/:idCliente', async (req, res) => {
   }
 });
 
+
+app.post('/pedido', async (req, res) => {
+  const { id_cliente, valor_total, forma_pagamento } = req.body;
+
+  try {
+    if (!id_cliente || !valor_total || !forma_pagamento) {
+      return res.status(400).json({ status: 'erro', erro: 'Dados obrigatórios faltando.' });
+    }
+
+    const resultado = await registrarResumoPedido(id_cliente, valor_total, forma_pagamento);
+
+    if (resultado.status === 'ok') {
+      res.json(resultado);
+    } else {
+      res.status(500).json(resultado);
+    }
+
+  } catch (error) {
+    console.error('Erro na rota /pedido:', error);
+    res.status(500).json({ status: 'erro', erro: 'Erro interno ao registrar pedido.' });
+  }
+});
+
+
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em: http://localhost:${port}`);
 });
